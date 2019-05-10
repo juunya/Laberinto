f48041ee8e2cef99fe678bca64f1fc034173626e
juunya <xnp8788fc@gmail.com>
2019-05-10 21:48:14 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("")
+public class MainController {
+
+    @GetMapping("/hello")
+    public String getTime(Model model){
+
+        System.out.println("--------------------------");
+        model.addAttribute("message", "Hello Thymeleaf!!");
+
+        return "index";
+
+    }
+
+}
+
